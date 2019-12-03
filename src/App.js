c92762e59820d5ddf3539d460215412965c97e2c
robsalzberg <robsalzberg@gmail.com>
2019-12-02 23:07:30 -0500

    Capitalize the S in Salzberg

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This will be Rob salzberg's Portfolio.
+          This will be Rob Salzberg's Portfolio.
         </p>
         <a
           className="App-link"
